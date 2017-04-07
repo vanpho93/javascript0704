a7d89512007d045e98ccbbeae500e19d130f4d5d
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-07 20:52:09 +0700

    abcd

---
+++ index.js
@@ -0,0 +1,13 @@
+// let a = 'Hello world';
+// a = 1000;
+// console.log(a != '1000');
+let c = 5;
+let d = 10;
+// let b = false;
+// let a = true;
+console.log(c > 100 & ++d > 5);
+console.log(d);
+
+if ('0'){
+    console.log('TRUEEEE')
+}
---
+++ try.js
@@ -0,0 +1,13 @@
+let a = 10;
+// console.log((a++, ++a));
+console.log(typeof a);
+
+const b = 20;
+
+// if(a > b) {
+//     console.log(a)
+// } else {
+//     console.log(b);
+// }
+
+console.log( a > b ? a : b );
